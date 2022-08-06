0913ded777256786c401249dae9e87b956528a4e
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-08-06 11:32:49 -0300

    About view

---
+++ workshop-javafx-jdbc/src/gui/MainViewController.java
@@ -0,0 +1,71 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemSeller;
+
+	@FXML
+	private MenuItem menuItemDepartment;
+
+	@FXML
+	private MenuItem menuItemAbout;
+
+	public void onMenuItemSellerAction() {
+		System.out.println("onMenuItemSellerAction");
+	}
+
+	public void onMenuItemSellerDepartmentAction() {
+		System.out.println("onMenuItemSellerDepart");
+	}
+
+	public void onMenuItemSellerAboutAction() {
+		loadView("/gui/AboutView.fxml");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+
+	}
+
+	//"synchronized" garante que o processamento de todo este método não será interrompido durante o multithreading
+	private synchronized void loadView(String absoluteName) {
+		try {
+//Para carregar uma view dentro da VBox principal, temos que pegar ela e transformar em uma Children/filha do VBoxPrincipal
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();	
+			VBox mainVbox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();	
+			
+			//Pega o filho da VBox principal na posiçao 0 que no caso é o MenuBar
+			Node mainMenu = mainVbox.getChildren().get(0);	
+			
+			mainVbox.getChildren().clear(); //Limpou todos os filhos 
+			mainVbox.getChildren().add(mainMenu);	//Adicionou novamente o MenuBar
+			mainVbox.getChildren().addAll(newVBox.getChildren());	//Adicionou todos os filhos da VBox da outra tela como filhos da VBox principal
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Excepetion", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+}
