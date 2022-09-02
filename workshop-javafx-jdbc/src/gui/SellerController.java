d5d1a1e5055dadf65d095a4e8b8d312b24585d91
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-09-02 09:45:25 -0300

    Seller Form

--- workshop-javafx-jdbc/src/gui/SellerController.java
+++ workshop-javafx-jdbc/src/gui/SellerController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Comparator;
 import java.util.Date;
@@ -17,7 +18,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -25,6 +28,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -110,36 +115,36 @@ public class SellerController implements Initializable, DataChangeListener {
 	}
 
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load(); // Carrega a view SellerForm
-//
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj);
-//			controller.setSellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this); // Se inscreve para receber o Evento (Assim fica
-//															// ouvindo/esperando para atualizar a tabela)
-//			controller.updateFormData();
-//
-//			// Cria um novo Stage/Palco que vai ficar por cima do Stage principal
-//			Stage dialogStage = new Stage();
-//			dialogStage.setTitle("Enter department data");
-//			dialogStage.setScene(new Scene(pane)); // Passa o form como cena
-//			dialogStage.setResizable(false); // Nao pode redimensionar a janela
-//			dialogStage.initOwner(parentStage); // Informa quem é o "PAI" dessa janela
-//			dialogStage.initModality(Modality.WINDOW_MODAL);// Janela será Modal, ou seja, enquanto nao fechar ela, não
-//															// acessa outra janela
-//			dialogStage.setWidth(parentStage.getWidth()); // Seta o tamanho maximo igual ao tamanho da janela Pai mas
-//															// caso a janela seja maximida,
-//															// Fica limitada ao tamanhao abaixo
-//			dialogStage.setMaxWidth(720); // Seta a largura maxima em 720
-//			dialogStage.setMaxHeight(220); // Seta a altura maxima em 220
-//
-//			dialogStage.showAndWait();
-//
-//		} catch (IOException e) {
-//			Alerts.showAlert("IO Excpetion", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load(); // Carrega a view SellerForm
+
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj);
+			controller.setSellerService(new SellerService());
+			controller.subscribeDataChangeListener(this); // Se inscreve para receber o Evento (Assim fica
+															// ouvindo/esperando para atualizar a tabela)
+			controller.updateFormData();
+
+			// Cria um novo Stage/Palco que vai ficar por cima do Stage principal
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Seller data");
+			dialogStage.setScene(new Scene(pane)); // Passa o form como cena
+			dialogStage.setResizable(false); // Nao pode redimensionar a janela
+			dialogStage.initOwner(parentStage); // Informa quem é o "PAI" dessa janela
+			dialogStage.initModality(Modality.WINDOW_MODAL);// Janela será Modal, ou seja, enquanto nao fechar ela, não
+															// acessa outra janela
+			dialogStage.setWidth(parentStage.getWidth()); // Seta o tamanho maximo igual ao tamanho da janela Pai mas
+															// caso a janela seja maximida,
+															// Fica limitada ao tamanhao abaixo
+			dialogStage.setMaxWidth(720); // Seta a largura maxima em 720
+			dialogStage.setMaxHeight(220); // Seta a altura maxima em 220
+
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Excpetion", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
---
+++ workshop-javafx-jdbc/src/gui/SellerFormController.java
@@ -0,0 +1,141 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+	
+	private SellerService departmentService;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	private Seller entity;
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelError;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSellerService (SellerService departmentService) {
+		this.departmentService = departmentService;
+	}
+	
+	public void setSeller (Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void subscribeDataChangeListener (DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null!");
+		}
+		if(departmentService == null) {
+			throw new IllegalStateException("Seller Service was null!");
+		}
+		
+		try {
+			entity = getFormData();		
+			departmentService.saveOrUpdate(entity);
+			Alerts.showAlert("Salve complete", null, "Seller salve success", AlertType.INFORMATION);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();		//Fecha a janela 
+			
+		}		
+		catch (ValidationException e) {
+			setErrorMessages(e.getErros());
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChange();
+		}
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		ValidationException exception = new ValidationException("Validation Error");
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");		
+		}
+			
+		obj.setName(txtName.getText());
+		
+		//Lança a exceção no final pois caso tenha varios campos e algum deles adcionasse um erro, lançaria o erro de todos
+		if (exception.getErros().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();		//Fecha a janela 
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {		
+		initializeNodes();
+	}
+
+	private void initializeNodes() {		
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);;
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null!");
+		}
+		txtId.setText(String.valueOf(entity.getId()));	//Precisa converter para String pois o TextField so aceita tipo String
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages (Map<String, String> erros) {
+		for (String key : erros.keySet()) {
+			if (key.equals("name")) {
+				labelError.setText(erros.get(key));
+			}
+		}
+	}
+}
