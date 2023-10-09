b2269c8698b671cfb622ae0a2f32ec6ee344d7c9
jorge-rufino <jorge.rufino1986@gmail.com>
2023-10-09 14:08:59 -0300

    Criando projeto JrParking

---
+++ jrparking/src/main/java/com/rufino/jrparking/JrparkingApplication.java
@@ -0,0 +1,13 @@
+package com.rufino.jrparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrparkingApplication.class, args);
+	}
+
+}
---
+++ jrparking/src/test/java/com/rufino/jrparking/JrparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rufino.jrparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
