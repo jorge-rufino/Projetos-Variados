5f970a99f18d6b940257871e883295a363a073af
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-06 08:55:29 -0300

    Chaveando o menu ao botao

---
+++ frontend/js/app.js
@@ -0,0 +1,7 @@
+var alternador = document.querySelector('.js-botao-chaveador');
+
+//Quando clicar no botão, vai adicionar/remover a classe "menu--exibindo", que faz com que o menu apareça
+alternador.onclick = function(){
+  var menu = document.querySelector('.js-menu');
+  menu.classList.toggle('menu--exibindo');  //Se já existir a classe ele remove, se não existir ele adiciona
+}
