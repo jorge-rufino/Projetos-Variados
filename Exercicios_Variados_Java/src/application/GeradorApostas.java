8ad2973a5358bf0c09e83627be38b2297ecbc1e3
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-31 15:18:22 -0300

    Gerador de Apostas alterado para lógica recursiva

--- Exercicios_Variados_Java/src/application/GeradorApostas.java
+++ Exercicios_Variados_Java/src/application/GeradorApostas.java
@@ -1,62 +1,46 @@
 package application;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class GeradorApostas {
 
 	public static void main(String[] args) {
 
-        int quantidadeApostas = 80;
+        int quantidadeApostas = 50;
         int numerosPorAposta = 6;
         int quantidadeNumerosSorteio = 60;
-        
-        int[][] apostas = gerarApostasMegaSena(quantidadeApostas, numerosPorAposta, quantidadeNumerosSorteio);
+        Set<Set<Integer>> apostas = gerarApostasMegaSena(quantidadeApostas, numerosPorAposta, quantidadeNumerosSorteio);
 
-        for (int i = 0; i < quantidadeApostas; i++) {
-            System.out.println(Arrays.toString(apostas[i]));
+        for (Set<Integer> aposta : apostas) {
+            System.out.println(aposta);
         }
     }
 
-	public static int[][] gerarApostasMegaSena(int quantidadeApostas, int numerosPorAposta, int quantidadeNumerosSorteio){
-		int [][] apostas = new int[quantidadeApostas][numerosPorAposta];
-		Set<String> apostasGeradas = new HashSet<>();
-		
-		Random random = new Random();
-		
-		for (int i = 0; i < quantidadeApostas; i++) {
+    public static Set<Set<Integer>> gerarApostasMegaSena(int quantidadeApostas, int numerosPorAposta, int quantidadeNumerosSorteio) {
+        Set<Set<Integer>> apostas = new HashSet<>();
+        Random random = new Random();
 
-            int[] novaAposta;
-            do {
-                novaAposta = new int[numerosPorAposta];
-                
-                for (int j = 0; j < numerosPorAposta; j++) {                	
-                	int numero;
-                	
-            		do {
-            			numero = random.nextInt(quantidadeNumerosSorteio) + 1;
-            		}while (contemNumero(novaAposta, numero));
-            		
-            		novaAposta[j] =  numero;
-                }
-                Arrays.sort(novaAposta);
-            } while (!apostasGeradas.add(Arrays.toString(novaAposta))); // Verifica duplicatas
-
-            apostas[i] = novaAposta;
-        }
+        gerarAposta(apostas, random, quantidadeApostas, numerosPorAposta, quantidadeNumerosSorteio);
 
         return apostas;
-	}
+    }
 
-	public static boolean contemNumero(int[] aposta, int numero) {
-		for (int i : aposta) {
-			if(i == numero) {
-				return true;
-			}
-		}
-		return false;
-	}
-    
+    private static void gerarAposta(Set<Set<Integer>> apostas, Random random, int quantidade, int numerosPorAposta, 
+    		int quantidadeNumerosSorteio) {
+    	
+        if (apostas.size() == quantidade) {
+            return; // Condição de parada
+        }
+
+        Set<Integer> novaAposta = new TreeSet<>(); // Usando TreeSet para manter a ordenação
+        while (novaAposta.size() < numerosPorAposta) {
+            novaAposta.add(random.nextInt(quantidadeNumerosSorteio) + 1);
+        }
+
+        apostas.add(novaAposta);
+        gerarAposta(apostas, random, quantidade, numerosPorAposta, quantidadeNumerosSorteio);
+    }
 }
