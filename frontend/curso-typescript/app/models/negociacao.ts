69059aca83258df5e7aaf847258ed39933abc2cd
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-15 11:00:13 -0300

    Alterando modificador de acesso para sintaxe TS

--- frontend/curso-typescript/app/models/negociacao.ts
+++ frontend/curso-typescript/app/models/negociacao.ts
@@ -1,45 +1,45 @@
 export class Negociacao {
-  //O "#" indica que os campos são privados. É o mesmo que usar o "private"
-  #data;
-  #quantidade;
-  #valor;
+  //O "_" é uma convenção javascript para indicar que os campos são privados. Não é obrigatório.
+  private _data;
+  private _quantidade;
+  private _valor;
 
   constructor(data: any, quantidade: any, valor: any){
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;  
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;  
   }
 
   //Temos 2 modos de fazer MÉTODOS "get"
   //Neste primeiro modo, quando formos chamar os métodos, iremos usar a sintaxe padrão de acessu a um atributo, sem parenteses.
   //Por exemplo: "negociacao.volume"
   get data(){
-    return this.#data;
+    return this._data;
   }
 
   get quantidade(){
-    return this.#quantidade;
+    return this._quantidade;
   }
 
   get valor(){
-    return this.#valor;
+    return this._valor;
   }
 
   //Perceba que volume é um método, não existe um atributo de classe "volume".
   get volume(){
-    return this.#quantidade * this.#valor;
+    return this._quantidade * this._valor;
   }
 
   //Aqui temos os métodos "get" de maneira convencional
   getData() {
-    return this.#data;
+    return this._data;
   }
 
   getQuantidade(){
-    return this.#quantidade;
+    return this._quantidade;
   }
 
   getValor(){
-    return this.#valor;
+    return this._valor;
   }
 }
