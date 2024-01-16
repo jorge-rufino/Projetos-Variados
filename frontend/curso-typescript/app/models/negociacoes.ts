090be57161d810bf4eb2307fb9b9938319d4414a
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-16 14:06:20 -0300

    Simplificando declaracao de tipagem de Arrays

--- frontend/curso-typescript/app/models/negociacoes.ts
+++ frontend/curso-typescript/app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private listaNegociacoes: Array<Negociacao> = [];
+  private listaNegociacoes: Negociacao[] = [];
 
   adicionar(negociacao: Negociacao): void{
     this.listaNegociacoes.push(negociacao);
   }
 
-  listar(): ReadonlyArray<Negociacao> {
+  listar(): readonly Negociacao[] {
     //Definindo o retorno como "ReadonlyArray", não será permitido o uso de metódos que modifiquem o array como o "pop" e "push"
     return this.listaNegociacoes;
   }
