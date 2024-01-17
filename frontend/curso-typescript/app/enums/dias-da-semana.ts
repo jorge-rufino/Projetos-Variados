048931660b6800017e29a70c62b28fe2918531d7
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-17 15:50:16 -0300

    Colocando valores nos dias da semana

--- frontend/curso-typescript/app/enums/dias-da-semana.ts
+++ frontend/curso-typescript/app/enums/dias-da-semana.ts
@@ -1,10 +1,11 @@
 //Por padrao o TS começa o index por 0, portanto os dias estão com os mesmos valores do metodo "getDay"
+//Definimos o valor manualmente para evitar erros em caso de alteração na ordem dos dias.
 export enum DiaDaSemana {
-  DOMINGO,
-  SEGUNDA,
-  TERÇA,
-  QUARTA,
-  QUINTA,
-  SEXTA,
-  SABADO
+  DOMINGO = 0,
+  SEGUNDA = 1,
+  TERÇA = 2,
+  QUARTA = 3,
+  QUINTA = 4,
+  SEXTA = 5,
+  SABADO = 6
 }
