ae7e7ea97027b6a5185b60efa4d4a2909dc7654e
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-17 16:55:43 -0300

    Protegendo o template contra scripts, usando parametro opcional

--- frontend/curso-typescript/app/views/view.ts
+++ frontend/curso-typescript/app/views/view.ts
@@ -6,16 +6,32 @@ export abstract class View<T> {
 
   //"Protected" para quem herdar poder acessar esta propriedade
   protected elemento: HTMLElement;
+  private escapar = false;
 
-  constructor(seletor: string) {
+  //Adicionamos o "?" no parametro "escapar" tornando ele OPCIONAL, assim não quebra o código obrigando
+  //que ele seja passado nas classes filhas.
+  //Poderiamos também passar um valor padrão para "escapar"(escapar: boolean=false), assim evitariamos de fazer o "if" e 
+  //ele continuaria sendo opcional.
+  constructor(seletor: string, escapar?: boolean) {
     this.elemento = document.querySelector(seletor) as HTMLElement;
+
+    if(escapar) {
+      this.escapar = escapar;
+    }
   }
 
   //Protected pois somente os filhos que são obrigados a implementa-lo devem ter acesso a ele.
   //Colocar "protected" nos filhos quando implementarem tb para restringir o acesso.
   protected abstract template(model: T): string;
 
+  //Agora nós estamos protegendo o template para caso alguem mal intencionado tente passar um script nele
+  //Ele seja removido
   public update(model: T): void {
-    this.elemento.innerHTML = this.template(model);
+    let template = this.template(model);
+    if(this.escapar){
+      template = template.replace(/<script>[\s\S]*?<\/script>/,'');
+    }
+
+    this.elemento.innerHTML = template;
   }
 }
